11827a73fc0f9f61393660ab6a49a62b983efc74
Helen Kang <helen@broadinstitute.org>
2026-06-09 23:32:21 +0000

    adding recent updates

--- workflow/rules/cNMF_pipeline.smk
+++ workflow/rules/cNMF_pipeline.smk
@@ -159,29 +159,33 @@ def get_rule_prepare_cNMF_partition(wildcards):
 # 		--minUniqueGenesPerCell {params.min_unique_genes_per_cell} ' "
 
 
-# def get_raw_h5ad_file(wildcards):
-# 	if os.path.isfile(config["input_h5ad_mtxDir"]):
-# 		return(config["input_h5ad_mtxDir"])
-# 	else:
-# 		return(os.path.join(config["analysisDir"], "data/raw/" + config["sampleName"] + ".h5ad"))
+def get_raw_h5ad_file(wildcards):
+	if os.path.isfile(config["input_h5ad_mtxDir"]):
+		return(config["input_h5ad_mtxDir"])
+	else:
+		return(os.path.join(config["analysisDir"], "data/raw/" + config["sampleName"] + ".h5ad"))
 
 
-# rule raw_h5ad_to_filtered_h5ad:
-# 	input:
-# 		raw_h5ad_mtx = get_raw_h5ad_file
-# 	output:	
-# 		h5ad_mtx = os.path.join(config["analysisDir"], "data/" + config["sampleName"] + ".h5ad"),
-# 		gene_name_txt = os.path.join(config["analysisDir"], "data/" + config["sampleName"] + ".h5ad.all.genes.txt")
-# 	params:
-# 		time = "2:00:00",
-# 		mem_gb = "64"
-# 	shell:
-# 		"bash -c ' source $HOME/.bashrc; \
-# 		conda activate cnmf_env; \
-# 		python workflow/scripts/filter_to_h5ad.py \
-# 		--inputPath {input.raw_h5ad_mtx} \
-# 		--output_h5ad {output.h5ad_mtx} \
-# 		--output_gene_name_txt {output.gene_name_txt} ' "
+rule raw_h5ad_to_filtered_h5ad:
+	input:
+		raw_h5ad_mtx = get_raw_h5ad_file
+	output:	
+		h5ad_mtx = os.path.join(config["analysisDir"], "data/" + config["sampleName"] + ".h5ad"),
+		gene_name_txt = os.path.join(config["analysisDir"], "data/" + config["sampleName"] + ".h5ad.all.genes.txt"),
+		barcode_names = os.path.join(config["analysisDir"], "data/" + config["sampleName"] + ".barcodes.txt")
+	params:
+		time = "2:00:00",
+		mem_gb = "64",
+		organism = config["organism"]
+	shell:
+		"bash -c ' source $HOME/.bashrc; \
+		conda activate cnmf_env; \
+		python workflow/scripts/filter_to_h5ad.py \
+		--inputPath {input.raw_h5ad_mtx} \
+		--output_h5ad {output.h5ad_mtx} \
+		--output_gene_name_txt {output.gene_name_txt} \
+		--output_barcodes {output.barcode_names} \
+		--organism {params.organism} ' "
 
 
 # rule raw_h5ad_to_filtered_h5ad_helper:
--- workflow/scripts/filter_to_h5ad.py
+++ workflow/scripts/filter_to_h5ad.py
@@ -5,6 +5,7 @@
 import os
 import pandas as pd
 import numpy as np
+import anndata as ad
 from scipy.io import mmread
 import scipy.sparse as sp
 # import matplotlib.pyplot as plt
@@ -19,6 +20,8 @@ parser.add_argument('--inputPath', dest = 'inputPath', type=str, default='/oak/s
 # parser.add_argument('--run_name',dest ='run_name', type=str, help = 'sample name')
 parser.add_argument('--output_h5ad_mtx', dest = 'output_h5ad_mtx', type=str, help = 'path to output folder', default='/oak/stanford/groups/engreitz/Users/kangh/TeloHAEC_Perturb-seq_2kG/211011_Perturb-seq_Analysis_Pipeline_scratch/211014_test_txt_to_h5ad/outputs/test.h5ad')
 parser.add_argument('--output_gene_name_txt', dest = 'output_gene_name_txt', type=str, help = 'path to gene name output txt file', default='/oak/stanford/groups/engreitz/Users/kangh/TeloHAEC_Perturb-seq_2kG/211011_Perturb-seq_Analysis_Pipeline_scratch/211014_test_txt_to_h5ad/outputs/test.h5ad.all.genes.txt')
+parser.add_argument('--output_barcodes', dest = 'output_barcodes', type=str, help = 'path to output CBC and sample barcodes table')
+parser.add_argument('--organism', dest = 'organism', type=str, default='human', help = 'organism (human or mouse); selects the non-protein-coding gene name patterns to remove')
 args = parser.parse_args()
 
 
@@ -37,15 +40,22 @@ args = parser.parse_args()
 ## load scanpy from txt
 adata = sc.read(args.inputPath)
 
-# remove non protein coding genes
+# remove non protein coding genes (gene name patterns depend on organism)
 df_columns = pd.Series(adata.var_names.values.flatten()).astype(str)
-AL = df_columns[df_columns.str.contains('^AL[0-9][0-9][0-9][0-9][0-9][0-9]\\.').tolist()]
-AC = df_columns[df_columns.str.contains('^AC[0-9][0-9][0-9][0-9][0-9][0-9]\\.').tolist()]
-AP = df_columns[df_columns.str.contains('^AP[0-9][0-9][0-9][0-9][0-9][0-9]\\.').tolist()]
-LINC = df_columns[df_columns.str.contains('LINC').tolist()]
-allPattern = df_columns[df_columns.str.contains('^[A-Za-z][A-Za-z][0-9][0-9][0-9][0-9][0-9][0-9]\\.').tolist()]
-toremove = allPattern.append(LINC)
-tokeep = ~df_columns.isin(toremove)
+organism = args.organism.lower()
+if organism == "mouse":
+    # mouse (MGI): predicted gene models, RIKEN cDNA clones, clone-named genes
+    Gm = df_columns.str.contains('^Gm[0-9]+$')                                     # e.g. Gm1992
+    Rik = df_columns.str.contains('Rik[0-9]*$')                                    # e.g. A930006A01Rik
+    clone = df_columns.str.contains('^[A-Z][A-Z][0-9][0-9][0-9][0-9][0-9][0-9]$')  # e.g. AI597479
+    tokeep = ~(Gm | Rik | clone)
+elif organism == "human":
+    # human: Ensembl clone-based names (2-letter + 6-digit + version) and lincRNAs
+    clone = df_columns.str.contains('^[A-Za-z][A-Za-z][0-9][0-9][0-9][0-9][0-9][0-9]\\.')  # e.g. AL123456.1
+    LINC = df_columns.str.contains('LINC')
+    tokeep = ~(clone | LINC)
+else:
+    raise ValueError("--organism must be 'human' or 'mouse', got: " + args.organism)
 adata = adata[:,tokeep]
 # filter cells
 sc.pp.filter_cells(adata, min_genes=200) # filter cells with fewer than 200 genes
@@ -60,3 +70,8 @@ filtered_genes = pd.DataFrame(adata.var_names.values.flatten()).astype(str)
 
 # save gene names
 filtered_genes.to_csv(args.output_gene_name_txt, sep="\t", header=False, index=False)
+
+# get CBC and Sample table for batch quantification
+obs_out = adata.obs.copy()
+obs_out.insert(0, "CBC", obs_out.index.astype(str))
+obs_out[["CBC", "sample"]].to_csv(args.output_barcodes, sep="\t", index=False)
